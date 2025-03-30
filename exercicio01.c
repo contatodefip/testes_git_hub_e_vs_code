9e66cb60d2530635911a678a3600b009c9a3904a
DEFIP <contatodefip@gmail.com>
2025-03-30 17:32:27 -0300

    atualizacao-4-pc

--- exercicio01.c
+++ exercicio01.c
@@ -3,12 +3,18 @@
 // EXERCÍCIOS FEITOS EM LIGUAGEM C PARA TESTAR INTEGRAÇÃO ENTRE VS CODE E GITHUB
 int main() {
 // 1. Iniciando o código vazio.
+
 // 2. Adicionando variáveis
-int numero1, numero2, numero3;
+int numero1, numero2, numero3, resultado1, resultado2;
 
-// Adicionando dados nas variáveis
-
+// 3. Adicionando dados nas variáveis
 numero1 = 10, numero2 = 20, numero3 = 30;
+resultado1 = numero1 * numero2;
+resultado2 = numero1 - numero2;
+
+// 4. Fazendo operações matemáticas
+printf("O resultado de %d x %d é: %d\n", numero1, numero2, resultado1);
+printf("O resultado de %d - %d é: %d\n", numero1, numero2, resultado2);
 
 return 0;
 
