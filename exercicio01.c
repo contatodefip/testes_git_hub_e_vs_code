f4bfe6bb472ac119f05954e960fcbabd0662a62c
DEFIP <contatodefip@gmail.com>
2025-03-30 17:18:51 -0300

    atualizacao-2-pc

--- exercicio01.c
+++ exercicio01.c
@@ -3,6 +3,8 @@
 // EXERCÍCIOS FEITOS EM LIGUAGEM C PARA TESTAR INTEGRAÇÃO ENTRE VS CODE E GITHUB
 int main() {
 // 1. Iniciando o código vazio.
+// 2. Adicionando variáveis
+int numero1, numero2, numero3;
 
 
 return 0;
